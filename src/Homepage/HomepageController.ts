7a48e6fe111d14689d734763ffce65f45f77e948
Michael Zabka <zabka.michael@gmail.com>
2015-08-23 20:52:57 +0200

    Add homepage controller

---
+++ src/Homepage/HomepageController.ts
@@ -0,0 +1,22 @@
+
+import express = require('express');
+
+export = HomepageController;
+class HomepageController {
+	
+	static $service = 'Homepage.HomepageController';
+	static $inject = [
+		'express.Express',
+	];
+	static $run = true;
+	constructor(
+		private app: express.Express
+	) {
+		this.app.get('/', (req: express.Request, res: express.Response) => this.indexAction(req, res));
+	}
+
+	private indexAction(req: express.Request, res: express.Response) {
+		res.send({});
+	}
+
+}
--- src/Server.ts
+++ src/Server.ts
@@ -1,10 +1,19 @@
 
+import express = require('express');
+
 export = Server;
 class Server {
 
 	static $service = 'Server';
+	static $inject = [
+		'express.Express',
+	];
+	constructor(
+		private app: express.Express
+	) {}
 
 	start() {
 		console.log('Start Server');
+		this.app.listen(process.env.PORT || 80);
 	}
 }
